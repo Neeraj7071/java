46873e055781e4528c5a2b5ca4ebfd53b1073212
Neeraj Khajuriya <97509661+Neeraj7071@users.noreply.github.com>
2022-04-27 01:35:31 +0530

    update

--- unit5/week3/day11/MainEmployee.java
+++ unit5/week3/day11/MainEmployee.java
@@ -7,9 +7,8 @@ import java.util.TreeSet;
 public class MainEmployee {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		Set<Employee> Employee=new TreeSet<Employee>(new Employee());
-		boolean check=true;
-		while(check) {
+		Set<Employee> employee=new TreeSet<Employee>(new Employee());
+		while(employee.size()<4) {
 			System.out.println("Employee Id");
 			int Id=sc.nextInt();
 			System.out.println("Employee name");
@@ -17,13 +16,9 @@ public class MainEmployee {
 			System.out.println("Employee salary");
 			double salary = sc.nextDouble();
 			Employee e=new Employee(Id, Name, salary);
-			Employee.add(e);
-			System.out.println("enter y if u want to exit and any key");
-			String ans=sc.next();
-			if(ans.charAt(0)=='y')
-				check=false;
+			employee.add(e);
 		}
-		for(Employee i:Employee) {
+		for(Employee i:employee) {
 			System.out.println(i);
 		}
 	}
